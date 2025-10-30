34996ee19a27b7b1231ccf4ee71b35c1925ece69
Satendra Kumar <satendrakm27@gmail.com>
2025-10-30 18:59:52 +0530

    Polynomial: add function to add tow polynomial

--- polynomial/main.cpp
+++ polynomial/main.cpp
@@ -40,11 +40,53 @@ void display(struct Poly p)
     printf("\n");
 }
 
+struct Poly *add(struct Poly *p1, struct Poly *p2)
+{
+    int i, j, k;
+    struct Poly *sum;
+    sum = (struct Poly *)malloc(sizeof(struct Poly));
+    sum->terms = (struct Term *)malloc((p1->n + p2->n) * sizeof(struct Term));
+    i = j = k = 0;
+    while (i < p1->n && j < p2->n)
+    {
+        if (p1->terms[i].exp > p2->terms[j].exp)
+        {
+            sum->terms[k++] = p1->terms[i++];
+        }
+        else if (p1->terms[i].exp < p2->terms[j].exp)
+        {
+            sum->terms[k++] = p1->terms[j++];
+        }
+        else
+        {
+            sum->terms[k].exp = p1->terms[i].exp;
+            sum->terms[k++].coeff = p1->terms[i++].coeff + p2->terms[j++].coeff;
+        }
+    }
+    for (; i < p1->n; i++)
+    {
+        sum->terms[k++] = p1->terms[i];
+    }
+    for (; j < p2->n; j++)
+    {
+        sum->terms[k++] = p2->terms[j];
+    }
+    sum->n = k;
+    return sum;
+}
+
 int main()
 {
-    struct Poly p1;
+    struct Poly p1, p2, *p3;
     create(&p1);
+    create(&p2);
+    p3 = add(&p1, &p2);
+    printf("\n");
     display(p1);
+    printf("\n");
+    display(p2);
+    printf("\n");
+    display(*p3);
 
     free(p1.terms);
     return 0;
