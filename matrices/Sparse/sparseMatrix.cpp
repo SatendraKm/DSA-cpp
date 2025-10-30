511084ac5868c598e44852fc75722392bacccdc5
Satendra Kumar <satendrakm27@gmail.com>
2025-10-30 15:44:50 +0530

    Matrices: "Sparse Matrix"- added a function to add two sparse matrix and return the sum of the matrix

--- matrices/Sparse/sparseMatrix.cpp
+++ matrices/Sparse/sparseMatrix.cpp
@@ -52,10 +52,67 @@ void Display(struct Sparse s)
         printf("\n");
     }
 }
+struct Sparse *add(struct Sparse *s1, struct Sparse *s2)
+{
+    struct Sparse *sum;
+    int i, j, k;
+    i = j = k = 0;
+    sum = (struct Sparse *)malloc(sizeof(struct Sparse));
+    sum->ele = (struct Element *)malloc((s1->num + s2->num) * sizeof(struct Element));
+    while (i < s1->num && j < s2->num)
+    {
+        if (s1->ele[i].i < s2->ele[j].i)
+        {
+            sum->ele[k++] = s1->ele[i++];
+        }
+        else if (s1->ele[i].i > s2->ele[j].i)
+        {
+            sum->ele[k++] = s2->ele[j++];
+        }
+        else
+        {
+            if (s1->ele[i].j < s2->ele[j].j)
+            {
+                sum->ele[k++] = s1->ele[i++];
+            }
+            else if (s1->ele[i].j > s2->ele[j].j)
+            {
+                sum->ele[k++] = s2->ele[j++];
+            }
+            else
+            {
+                sum->ele[k] = s1->ele[i];
+                sum->ele[k++].x = s1->ele[i++].x + s2->ele[j++].x;
+            }
+        }
+    }
+    for (; i < s1->num; i++)
+    {
+        sum->ele[k++] = s1->ele[i];
+    }
+    for (; j < s2->num; j++)
+    {
+        sum->ele[k++] = s2->ele[j];
+    }
+    sum->m = s1->m;
+    sum->n = s1->n;
+    sum->num = k;
+
+    return sum;
+}
+
 int main()
 {
-    struct Sparse s;
-    create(&s);
-    Display(s);
+    struct Sparse s1, s2, *s3;
+    create(&s1);
+    create(&s2);
+    s3 = add(&s1, &s2);
+    printf("First Matrix: \n");
+    Display(s1);
+    printf("Second Matrix: \n");
+    Display(s2);
+    printf("Sum Matrix: \n");
+    Display(*s3);
+
     return 0;
 }
