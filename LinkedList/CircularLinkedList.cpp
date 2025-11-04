60e43f9927e3f4d4cc8580f1906c8c6ce300dbf0
Satendra Kumar <satendrakm27@gmail.com>
2025-11-04 11:27:16 +0530

    LinkedList(Circular): function to insert element into the list

--- LinkedList/CircularLinkedList.cpp
+++ LinkedList/CircularLinkedList.cpp
@@ -44,10 +44,64 @@ void RDisplay(struct Node *h)
     }
     flag = 0;
 }
+int Length(struct Node *p)
+{
+    int len = 0;
+    do
+    {
+        len++;
+        p = p->next;
+    } while (p != Head);
+    return len;
+}
+
+// Note: function to insert elements in a circular linked list
+void Insert(struct Node *p, int index, int x)
+{
+    struct Node *t;
+    int i;
+    if (index < 0 || index > Length(p))
+    {
+        return;
+    }
+
+    if (index == 0)
+    {
+        t = (struct Node *)malloc(sizeof(struct Node));
+        t->data = x;
+        if (Head == NULL)
+        {
+            Head = t;
+            Head->next = Head;
+        }
+        else
+        {
+            while (p->next != Head)
+            {
+                p = p->next;
+            }
+            p->next = t;
+            t->next = Head;
+            Head = t;
+        }
+    }
+    else
+    {
+        for (i = 0; i < index - 1; i++)
+        {
+            p = p->next;
+        }
+        t = (struct Node *)malloc(sizeof(struct Node));
+        t->data = x;
+        t->next = p->next;
+        p->next = t;
+    }
+}
 int main()
 {
     int A[] = {2, 3, 4, 5, 6};
     create(A, 5);
+    Insert(Head, 2, 10);
     Display(Head);
 
     return 0;
