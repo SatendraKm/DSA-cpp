268395bd11226c71b056836a96830c52b25b5c96
Satendra Kumar <satendrakm27@gmail.com>
2025-11-04 12:04:17 +0530

    LinkedList(Circular): delete function to delete an element from the circular linked list

--- LinkedList/CircularLinkedList.cpp
+++ LinkedList/CircularLinkedList.cpp
@@ -97,11 +97,53 @@ void Insert(struct Node *p, int index, int x)
         p->next = t;
     }
 }
+// Note: Function to delete an element from the circular linked list
+int Delete(struct Node *p, int index)
+{
+    struct Node *q;
+    int i, x;
+    if (index < 0 || index > Length(p))
+    {
+        return -1;
+    }
+    if (index == 1)
+    {
+        while (p->next != Head)
+        {
+            p = p->next;
+        }
+        x = Head->data;
+        if (Head == p)
+        {
+            free(Head);
+            Head == NULL;
+        }
+        else
+        {
+            p->next = Head->next;
+            free(Head);
+            Head = p->next;
+        }
+    }
+    else
+    {
+        for (i = 0; i < index - 2; i++)
+        {
+            p = p->next;
+        }
+        q = p->next;
+        p->next = q->next;
+        x = q->data;
+        free(q);
+    }
+    return x;
+}
 int main()
 {
     int A[] = {2, 3, 4, 5, 6};
     create(A, 5);
-    Insert(Head, 2, 10);
+    // Insert(Head, 2, 10);
+    printf("Deleted element- %d\n", Delete(Head, 3));
     Display(Head);
 
     return 0;
