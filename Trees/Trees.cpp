6a83aa019b028f6d1a1d89365c89dea67b67cdfb
SatendraKm <satendrakm27@gmail.com>
2025-11-14 19:07:57 +0530

    Trees: LevelOrder traversing code implemented

--- Trees/Trees.cpp
+++ Trees/Trees.cpp
@@ -148,11 +148,33 @@ void IPostorder(struct Node *p)
     }
 }
 
+void LevelOrder(struct Node *root)
+{
+    struct Queue q;
+    create(&q, 100);
+    printf("%d ", root->data);
+    enqueue(&q, root);
+    while (!isEmpty(&q))
+    {
+        root = dequeue(&q);
+        if (root->lchild)
+        {
+            printf("%d ", root->lchild->data);
+            enqueue(&q, root->lchild);
+        }
+        if (root->rchild)
+        {
+            printf("%d ", root->rchild->data);
+            enqueue(&q, root->rchild);
+        }
+    }
+}
+
 int main()
 {
     TreeCreate();
-    printf("\nPreorder Traversal: ");
-    IInorder(root);
+    printf("\nLevelOrder Traversal: ");
+    LevelOrder(root);
     printf("\n");
     return 0;
 }
