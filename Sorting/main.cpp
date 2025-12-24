317bb887f574fc78b1652b2e7f6f8b88d5afdbc5
SatendraKm <satendrakm27@gmail.com>
2025-12-25 01:07:48 +0530

    Sorting: Implemented Merge function to merge two list using an auxiliary array and also added an Iterativer function to MergeSort the elements of the array (2-way merge sort)

--- Sorting/main.cpp
+++ Sorting/main.cpp
@@ -97,14 +97,64 @@ void QuickSort(int A[], int l, int h)
     }
 }
 
+void Merge(int A[], int l, int mid, int h)
+{
+    int i = l, j = mid + 1, k = l;
+    int B[h + 1];
+    while (i <= mid && j <= h)
+    {
+        if (A[i] < A[j])
+        {
+            B[k++] = A[i++];
+        }
+        else
+        {
+            B[k++] = A[j++];
+        }
+    }
+    for (; i <= mid; i++)
+    {
+        B[k++] = A[i];
+    }
+    for (; j <= h; j++)
+    {
+        B[k++] = A[j];
+    }
+    for (i = l; i <= h; i++)
+    {
+        A[i] = B[i];
+    }
+}
+
+void IMergeSort(int A[], int n)
+{
+    int p, l, h, mid, i;
+    for (p = 2; p <= n; p = p * 2)
+    {
+        for (i = 0; i + p - 1 < n; i = i + p)
+        {
+            l = i;
+            h = i + p - 1;
+            mid = (l + h) / 2;
+            Merge(A, l, mid, h);
+        }
+    }
+    if (p / 2 < n)
+    {
+        Merge(A, 0, p / 2 - 1, n - 1);
+    }
+}
+
 int main()
 {
-    int A[] = {11, 13, 7, 12, 16, 9, 24, 5, 10, 3, INT_MAX}, n = 11, i;
+    // int A[] = {11, 13, 7, 12, 16, 9, 24, 5, 10, 3, INT_MAX}, n = 11, i;  // Int_max is for quicksort algorithm
+    int A[] = {11, 13, 7, 12, 16, 9, 24, 5, 10, 3}, n = 10, i;
 
     // BubbleSort(A, n);
     // InsertionSort(A, n);
     // SelectionSort(A, n);
-    QuickSort(A, 0, 10);
+    // QuickSort(A, 0, 10);
+    IMergeSort(A, 10);
 
     for (i = 0; i < 10; i++)
     {
